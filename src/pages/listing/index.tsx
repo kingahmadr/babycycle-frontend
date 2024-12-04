57b9cfe72a8f6524c1f1d53b68b50fd4e5fddbae
Adinda Maya <adnmaya02@gmail.com>
2024-12-04 22:43:14 +0700

    feat: added slicing for filters

--- src/pages/listing/index.tsx
+++ src/pages/listing/index.tsx
@@ -1,3 +1,4 @@
+import { PrimaryButton } from '@/components/PrimaryButton';
 import ProductCard from '@/components/ProductCard'
 import React, { useState } from 'react'
 
@@ -17,10 +18,114 @@ function index() {
 
   return (
     <div className='w-[1440px] flex'>
-      <div className='w-1/4'>
-        All Filters
+      <div className='fixed w-1/4 uppercase py-3 flex flex-col gap-6'>
+        <div className='text-xl text-buttonBlue'>All Filters</div>
+
+        <div className='flex flex-col gap-2 px-3'>
+          <div>Availability</div>
+          <div className='flex flex-col gap-1 text-sm'>
+            <label className='flex gap-2 items-center'>
+              <input type="radio"
+                name="availability"
+                value="available"/>
+              <span>Available</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="radio"
+                name="availability"
+                value="out_of_stock"/>
+              <span>Out of stock</span>
+            </label>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-2 px-3'>
+          <div>Product Type</div>
+          <div className='flex flex-col gap-1 text-sm'>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="category"
+                value="clothing"/>
+              <span>Clothing</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="category"
+                value="furniture"/>
+              <span>furniture</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="category"
+                value="stroller_and_carrier"/>
+              <span>stroller & carrier</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="category"
+                value="toys"/>
+              <span>toys</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="category"
+                value="others"/>
+              <span>others</span>
+            </label>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-2 px-3'>
+          <div>warranty</div>
+          <div className='flex flex-col gap-1 text-sm'>
+            <label className='flex gap-2 items-center'>
+              <input type="radio"
+                name="warranty"
+                value="yes"/>
+              <span>yes</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="radio"
+                name="warranty"
+                value="no"/>
+              <span>no</span>
+            </label>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-2 px-3'>
+          <div>Product Type</div>
+          <div className='flex flex-col gap-1 text-sm'>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="age_category"
+                value="0-2"/>
+              <span>0-2</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="age_category"
+                value="3-5"/>
+              <span>3-5</span>
+            </label>
+            <label className='flex gap-2 items-center'>
+              <input type="checkbox"
+                name="age_category"
+                value="all_ages"/>
+              <span>all ages</span>
+            </label>
+          </div>
+        </div>
+
+        <div className='pt-8'>
+          <PrimaryButton type='button'>Apply</PrimaryButton>
+        </div>
+
       </div>
-      <div className='w-3/4'>
+
+
+
+      <div className='w-3/4 ml-[25%]'>
         <div className='uppercase text-[14px] h-auto py-3 flex justify-end items-center'>
           <div className='flex items-center gap-3'>
 
