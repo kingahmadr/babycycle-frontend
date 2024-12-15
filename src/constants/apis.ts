adc6c3a1e976fdf78bd8da6d261999bd450c5669
kingahmadr <ahmadnromiz@gmail.com>
2024-12-15 07:44:52 +0700

    fix: auth feature

---
+++ src/constants/apis.ts
@@ -0,0 +1,6 @@
+export const API_URL = 'https://api.babycycle.my.id/api/v1'
+export const API_LOGIN = `${API_URL}/auth/login`
+export const API_REGISTER = `${API_URL}/users/register`
+export const API_RESEND = `${API_URL}/users/resend_verification`
+export const API_VERIFY = `${API_URL}/users/verify`
+export const API_ME = `${API_URL}/users/me`
