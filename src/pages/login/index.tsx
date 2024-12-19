4326cd389630fe42bacf9588fb8856b2746444e0
Adinda Maya <adnmaya02@gmail.com>
2024-12-20 01:10:00 +0700

    fix: responsive in login and register

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -43,45 +43,48 @@ export default function Login() {
 
   return (
       
-    <div className='w-full h-[calc(100vh-160px)] max-md:h-[calc(100vh-340px)] bg-[url(/Group_119.png)] max-md:bg-[url(/Hero2.png)] flex items-center justify-center'>
+    <div className='w-full bg-babyBlue max-md:bg-transparent flex justify-center'>
 
-      <div className='w-0 lg:w-1/2'></div>
-      <div className='p-6 rounded w-full lg:w-1/2 max-w-md'>
-        <>
-          <h1 className='text-4xl font-medium text-textBlue mb-4'>LOGIN</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type='email'
-              placeholder='Email'
-              value={form.email}
-              onChange={(e) => setForm({ ...form, email: e.target.value })}
-              required
-              className='w-full p-3 border mb-4 focus:ring-2 focus:ring-buttonBlue focus:outline-none'
-            />
-            <input
-              type='password'
-              placeholder='Password'
-              value={form.password}
-              onChange={(e) => setForm({ ...form, password: e.target.value })}
-              required
-              className='w-full p-3 border mb-4 focus:ring-2 focus:ring-buttonBlue focus:outline-none'
-            />
-            <button
-              type='submit'
-              className='w-full bg-buttonBlue hover:bg-textBlue text-white py-2 rounded transition-all duration-300'
-              disabled={loading}
-            >
-              {loading ? 'Loading...' : 'LOGIN'}
-            </button>
-          </form>
-          {error && <p className='text-red-500 text-sm mb-4'>{error}</p>}
-          <p className='italic text-sm mt-4'>
-            Don&apos;t have an account?{' '}
-            <Link href='/register' className='text-buttonBlue'>
-              Register here!
-            </Link>
-          </p>
-        </>
+      <div className='max-w-[1440px] h-[calc(100vh-160px)] max-md:h-[calc(100vh-340px)] bg-[url(/Group_119.png)] max-md:bg-[url(/Hero2.png)] bg-no-repeat bg-center max-md:bg-left object-contain w-full flex items-center justify-center'>
+
+        <div className='w-0 lg:w-1/2'></div>
+        <div className='p-6 rounded w-full lg:w-1/2 max-w-md'>
+          <>
+            <h1 className='text-4xl font-medium text-textBlue mb-4'>LOGIN</h1>
+            <form onSubmit={handleSubmit}>
+              <input
+                type='email'
+                placeholder='Email'
+                value={form.email}
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+                required
+                className='w-full p-3 border mb-4 focus:ring-2 focus:ring-buttonBlue focus:outline-none'
+              />
+              <input
+                type='password'
+                placeholder='Password'
+                value={form.password}
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+                required
+                className='w-full p-3 border mb-4 focus:ring-2 focus:ring-buttonBlue focus:outline-none'
+              />
+              <button
+                type='submit'
+                className='w-full bg-buttonBlue hover:bg-textBlue text-white py-2 rounded transition-all duration-300'
+                disabled={loading}
+              >
+                {loading ? 'Loading...' : 'LOGIN'}
+              </button>
+            </form>
+            {error && <p className='text-red-500 text-sm mb-4'>{error}</p>}
+            <p className='italic text-sm mt-4'>
+              Don&apos;t have an account?{' '}
+              <Link href='/register' className='text-buttonBlue'>
+                Register here!
+              </Link>
+            </p>
+          </>
+        </div>
       </div>
     </div>
   )
